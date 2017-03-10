a5cb236b4e4445871875b8759eeb8c156a93de98
Francesc Pineda Segarra <shawe.ewahs@gmail.com>
2017-03-10 01:49:20 +0100

    Commit inicial

---
+++ README.md
@@ -0,0 +1,5 @@
+# ayuda_devs
+
+No se trata de un plugin en si mismo, sino de una forma de tener una pequeña guía de estilos comunitaria que sirva de ayuda entre programadores.
+
+La intención de hacerla cómo plugin no es otra que facilitar la participación de otros programadores para aportar lo que consideren oportuno.
---
+++ controller/ayuda_estilo.php
@@ -0,0 +1,27 @@
+<?php
+
+/*
+ * This file is part of FacturaScripts
+ * Copyright (C) 2017  Francesc Pineda Segarra  shawe.ewahs@gmail.com
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+class ayuda_estilo extends fs_controller {
+
+   public function __construct() {
+      parent::__construct(__CLASS__, 'Estilo', 'Documentación', FALSE, TRUE);
+   }
+
+}
